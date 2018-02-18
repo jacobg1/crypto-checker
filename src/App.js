dadc82c74a2391bf25bc2d0aa7c08446bc0f2b0e
Jacob Greenwald <jeg4t@virginia.edu>
2018-02-18 14:10:50 -0500

    add prices

--- src/App.js
+++ src/App.js
@@ -72,13 +72,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="bitcoin">
+          <h1>Bitcoin</h1>
+          <p>{this.state.btc}</p>
+        </div>
+        <div className="bitcoinCash">
+          <h1>Bitcoin Cash</h1>
+          <p>{this.state.bch}</p>
+        </div>
+        <div className="etherum">
+          <h1>Etherum</h1>
+          <p>{this.state.eth}</p>
+        </div>
+        <div className="litecoin">
+          <h1>Litecoin</h1>
+          <p>{this.state.ltc}</p>
+        </div>
       </div>
     );
   }
